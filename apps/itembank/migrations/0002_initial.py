5886dfbbf1840f557d306c9bda5c722d096e4729
Kalayi <maximillankalayi001@gmail.com>
2026-05-21 16:48:31 +0000

    feat(flag_low_quality_items): enhance command logic and add tests for threshold validation

--- apps/itembank/migrations/0002_initial.py
+++ apps/itembank/migrations/0002_initial.py
@@ -8,8 +8,6 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    initial = True
-
     dependencies = [
         ('itembank', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
